32b033ff546e39ac35f399b72bf0f0eb922b7c1e
Alejandro Valdevila Pérez <avaldevilap@gmail.com>
2021-09-16 11:41:52 +0200

    fix: code clean

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -38,25 +38,3 @@ func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
 func (runtime Runtime) GormDataType() string {
 	return "int32"
 }
-
-// func (runtime *Runtime) Scan(value interface{}) error {
-// 	str := fmt.Sprintf("%v", value)
-// 	fmt.Println(str)
-// 	unquotedJSONValue, err := strconv.Unquote(string(str))
-// 	if err != nil {
-// 		return ErrInvalidRuntimeFormat
-// 	}
-// 	parts := strings.Split(unquotedJSONValue, " ")
-// 	if len(parts) != 2 || parts[1] != "mins" {
-// 		return ErrInvalidRuntimeFormat
-// 	}
-// 	i, err := strconv.ParseInt(parts[0], 10, 32)
-// 	if err != nil {
-// 		return ErrInvalidRuntimeFormat
-// 	}
-// 	*runtime = Runtime(i)
-// 	return nil
-// }
-// func (runtime Runtime) Value() (driver.Value, error) {
-// 	return int64(runtime), nil
-// }

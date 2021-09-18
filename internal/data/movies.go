601a1d695ca469b2d9ddb410b8b6ef088f0ac1e6
Alejandro Valdevila Pérez <avaldevilap@gmail.com>
2021-09-18 14:14:33 +0200

    feat: fulltext search in title field

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -7,10 +7,10 @@ import (
 
 type Movie struct {
 	gorm.Model
-	Title   string         `json:"title"`
+	Title   string         `json:"title" gorm:"index:,type:gin,expression:to_tsvector('english'\\,title)"`
 	Year    int32          `json:"year"`
 	Runtime Runtime        `json:"runtime" gorm:"type:int"`
-	Genres  pq.StringArray `json:"genres" gorm:"type:text[]"`
+	Genres  pq.StringArray `json:"genres" gorm:"type:text[],index:,type:gin"`
 	Version int32          `json:"version" gorm:"default:1"`
 }
 
@@ -47,15 +47,26 @@ func (m MovieModel) Delete(id int64) error {
 
 func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
 	movies := []*Movie{}
-	err := m.DB.Where(
-		"LOWER(title) = LOWER(@title) OR @title = ''",
-		map[string]interface{}{"title": title},
-	).Where(
-		"genres::text[] @> @genres OR @genres = '{}'",
-		map[string]interface{}{"genres": pq.Array(genres)},
-	).Find(&movies).Order("id DESC")
+	query := m.DB
+
+	if title != "" {
+		query = query.Where(
+			"to_tsvector('english', title) @@ plainto_tsquery('english', ?)",
+			title,
+		)
+	}
+
+	if len(genres) > 0 {
+		query = query.Where(
+			"genres::text[] @> ?",
+			pq.Array(genres),
+		)
+	}
+
+	err := query.Find(&movies).Order("id DESC")
 	if err.Error != nil {
 		return nil, err.Error
 	}
+
 	return movies, nil
 }
